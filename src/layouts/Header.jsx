82c341d088c5e5320c3db1045d3a96ded59c0531
intellltech <intelheropuck@gmail.com>
2025-02-14 09:38:40 -0800

    update header.jsx

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -5,9 +5,9 @@ export default function Header() {
 
   return (
     <div className="w-full top-0 fixed left-1/2 -translate-x-1/2 px-3 z-50 duration-200">
-      <div className="container mx-auto relative py-4 max-w-[800px]">
+      <div className="container mx-auto relative py-4 max-w-[900px]">
         <div className="flex justify-between">
-          <div className="logo relative items-center flex">
+          <div className="logo relative flex">
             <a
               href="/"
               className="absolute -top-1 left-0 h-[65px] w-[72px] hidden sm:inline-block"
@@ -15,6 +15,7 @@ export default function Header() {
               <img src="/logo.png" className="h-[70px] w-[75px]" alt="" />
             </a>
           </div>
+          <p className="text-3xl my-auto">Swap Page</p>
           <div className="nav_action">
             <WalletConnect />
           </div>
